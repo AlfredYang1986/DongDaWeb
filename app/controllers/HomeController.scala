5b993fc0dbcb1bc07da2b7b97e6ddf6f624bda24
AlfredYang1986 <yangyuanpig@gmail.com>
2018-06-30 17:32:13 +0800

    咚哒官网

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -9,6 +9,11 @@ class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder:
     extends AbstractController(cc) {
 
     def index = Action {
-        Ok(views.html.index("Your new application is ready"))
+//        Ok(views.html.index("Your new application is ready"))
+        Ok(views.html.index(assetsFinder))
+    }
+
+    def privacy = Action {
+        Ok(views.html.privacy(assetsFinder))
     }
 }
